8c3605a0e183edf02018af96823d29df28acf8e8
jaikwangg <kanna9764@gmail.com>
2025-07-23 14:23:30 +0700

    fix path config

--- vite-ui/src/App.tsx
+++ vite-ui/src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <AssessmentProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/baan-un-jai-planner">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
